f9d59c4942bd7217fbf2f09933c174e64bc88eb7
Dresandres34 <amurciag@ulagrancolombia.edu.co>
2021-09-20 21:29:07 -0500

    Creacion de metodo calculadora

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -3,8 +3,14 @@
     <h1>Mi primer componente</h1>
     <!-------Interpolación----->
     <h2>{{subtitulo}}</h2>
+    <!---crear imput para enviar parametros al metodo sumar---> 
+    <input type="number" id="num1" placeholder="Numero 1" v-model="numero_1"/>
+    <br>
+    <br>
+    <input type="number" id="num2" placeholder="Numero 2" v-model="numero_2"/>
+    <br>
     <!---crear boton para llamar metodo--->
-    <button @click="sumar">Sumar</button> 
+    <!--button @click="">Sumar</button>---->
   </div>
 </template>
 
@@ -13,12 +19,15 @@ export default {
     /*datos que manipulan el componente (variables globales)*/
     data: ()=> {
         return {
-            subtitulo: 'Calculadora'
+            subtitulo: 'Calculadora',
+            numero_1: null,
+            numero_2: null
         }
     },
     methods: {
-        sumar(){
+        sumar(numero_1, numero_2){
             console.log("metodo sumar")
+            console.log("Resultado: ",(numero_1+numero_2))
         }
     },
     created : ()=>{
